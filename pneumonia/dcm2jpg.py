0f9e38310cc2c6e0ab22ad44a3e2bff10c2649a3
mittshah2 <43903634+mittshah2@users.noreply.github.com>
2020-06-15 01:59:49 +0530

    Update dcm2jpg.py

--- pneumonia/dcm2jpg.py
+++ pneumonia/dcm2jpg.py
@@ -4,14 +4,14 @@ from skimage.transform import resize
 from tqdm import tqdm
 import pydicom as dicom
 
-def store(df_path,w=256,h=256):
+def store(train_images,df_path,w=256,h=256):
 
     os.mkdir('data')
     os.mkdir('data/positive')
     os.mkdir('data/negative')
     df = pd.read_csv(df_path)
 
-    df['path'] = df_path + df['patientId'].astype(str) + '.dcm'
+    df['path'] = train_images + df['patientId'].astype(str) + '.dcm'
 
     negative = df[df['Target'] == 0]
 
@@ -21,10 +21,7 @@ def store(df_path,w=256,h=256):
     patientId = unique_positive['patientId'].unique()
 
     unique_positive = pd.DataFrame({'path': path, 'patientId': patientId})
-
-    current_dir=os.getcwd()
-    os.chdir(current_dir)
-    print('cur--',current_dir)
+   
 
     for _, row in tqdm(unique_positive.iterrows()):
         img = dicom.read_file(row['path']).pixel_array
